16a5f6cf484d29adb4492f0c7c8fdadff2109e44
dantariun <dantariun@gmail.com>
2025-04-08 10:01:38 +0900

    #26.4 ffmpeg

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import {onRequest} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
-
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
@@ -27,5 +6,21 @@ admin.initializeApp();
 export const onVideoCreated = functions.firestore
 .document("videos/{videoId}")
 .onCreate(async (snapshot, context) => {
-    await snapshot.ref.update({hello : "from functions"});
+    const spawn = require('child-process-promise').spawn;
+    const video = snapshot.data();
+    await spawn("ffmpeg" , [
+        "-i",
+        video.fileUrl,
+        "-ss",
+        "00:00:01.000",
+        "-vframes",
+        "1",
+        "-vf",
+        "scale=150:-1",
+        `/tmp/${snapshot.id}.jpg`
+    ])
+    const storage = admin.storage();
+    storage.bucket().upload(`/tmp/${snapshot.id}.jpg` , {
+        destination: `thumbnails/${snapshot.id}.jpg`,
+    });
 });
